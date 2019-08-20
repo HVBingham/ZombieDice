b4c43eed3e8dd2eb791f0ad4c0bc344f99a57bb2
Hannah Bingham <binghan.hb@gmail.com>
2019-08-20 15:02:02 -0500

    added win or lose if statement, added counters for values of each round, cleaned up functions and took out things that were not useful

--- zombiedice.js
+++ zombiedice.js
@@ -1,35 +1,47 @@
-let rollCounter = 0;
-let shotCounter = 0;
-let brainsCounter = 0;
-let footCounter = 0;
-let carriedBrains = 0;
+	let brainsCounter = 0;
+	let shotCounter = 0;
+	let footCounter = 0;
+	let carriedBrains = 0;
+	let clicks=0;
+	
+
 function getRoll(){
-	if(confirm("Click for your roll?")){
-	rollAllDice();
-	console.log("Your total carried Brains for this round is " +carriedBrains)
-	if (carriedBrains === 10){
-			console.log("you Win!")
+	if(confirm("Click for your roll.")){
+		if(clicks <= 9){
+			rollAllDice();
+			console.log("Your total Brains after this round is " +carriedBrains)
+			let numberOfTurns = countTurns();
+			console.log("Round" +numberOfTurns);
+			if(carriedBrains >= 5 && clicks===10){
+				console.log("You win")		
 			}
-	else if (carriedBrains < 10){
-		console.log("you lose :( ")
-	}	
-	}
+			else if(carriedBrains < 5 && clicks ===10){
+				console.log("you lose")
+			}
+		}
+		}
 	else{
+		
 		alert("you didn't roll!")
+		}
 	}
-}
 
 function rollAllDice(){
-		let diceOne = counterBrains(rollSingleDice(3));
-		let diceTwo = counterBrains(rollSingleDice(4));
-		let diceThree = counterBrains(rollSingleDice(6));
-		let diceFour = counterBrains(rollSingleDice(8));
-		let diceFive = counterBrains (rollSingleDice(10));
-		let diceSix = counterBrains(rollSingleDice(12));
-		carriedBrains = brainsCounter - shotCounter;
+		counterBrains(rollSingleDice(3));
+		counterBrains(rollSingleDice(4));
+		counterBrains(rollSingleDice(6));
+		counterBrains(rollSingleDice(8));
+		counterBrains (rollSingleDice(10));
+		counterBrains(rollSingleDice(12));
+		console.log("your shots for this round is "+shotCounter)
+		console.log("your brains for this round is "+brainsCounter)
+		console.log("your feet for this round is "+footCounter)
+		carriedBrains = brainsCounter - shotCounter
 		return carriedBrains
 }
+
 function counterBrains(diceRoll){
+
 	if(diceRoll === 2){
 		return	++brainsCounter;
 	}
@@ -61,4 +73,21 @@ function counterBrains(diceRoll){
 function rollSingleDice(sides){
 	let roll = Math.ceil(Math.random()*sides);
 	return roll;
+}
+
+function countTurns(){
+	let counting = clicks +=1
+	return counting
+	
+}
+function winOrLose(){
+		if(carriedBrains == 5){
+		console.log("You Win!");
+	}
+	else {
+			console.log("You lose!")
+		}
+}
+function addEmUp(){
+	
 }
